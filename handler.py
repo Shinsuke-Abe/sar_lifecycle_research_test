9ea7868fa1bd2715cd1e019f99a33018325dfc96
Shinsuke-Abe <mao.instantlife@gmail.com>
2018-03-22 05:41:15 +0900

    2nd publish. add simpletable.

--- handler.py
+++ handler.py
@@ -1,5 +1,8 @@
+import os
+
 def hello(event, context):
     return {
         "message": "This function for research repository lifecycle",
-        "event": "hello!" 
+        "event": "hello!",
+        "database": os.environ['HELLO_TABLE'] 
     }
